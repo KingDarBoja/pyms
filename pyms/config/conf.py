8626d03c20d6595b65a562d31f1a92db81c9cf38
Alberto Vara <a.vara.1986@gmail.com>
2019-12-15 20:24:01 +0100

    Refactor

--- pyms/config/conf.py
+++ pyms/config/conf.py
@@ -2,7 +2,7 @@ from pyms.config.confile import ConfFile
 from pyms.exceptions import ServiceDoesNotExistException
 
 
-__service_configs = {}
+CONFIG = {}
 
 
 def get_conf(*args, **kwargs):
@@ -35,10 +35,11 @@ def get_conf(*args, **kwargs):
     :param kwargs:
     :return:
     """
+    global CONFIG
     service = kwargs.pop('service', None)
     memoize = kwargs.pop('memoize', True)
     if not service:
         raise ServiceDoesNotExistException("Service not defined")
-    if not memoize or service not in __service_configs:
-        __service_configs[service] = ConfFile(*args, **kwargs)
-    return getattr(__service_configs[service], service)
+    if not memoize or not CONFIG:
+        CONFIG = ConfFile(*args, **kwargs)
+    return getattr(CONFIG, service)
